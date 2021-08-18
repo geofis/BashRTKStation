8884eb714d8492abe69073b368d295ab351645a0
geofis <joseramon@geografiafisica.org>
2021-08-17 21:33:13 -0400

    Single script mode

--- seleccionar_modo_corr.sh
+++ seleccionar_modo_corr.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 # Bash Menu Script Example
 
-base_name="$(date +"%Y%m%d-%H%M%S")"
-corr_name="$base_name-rtcm3-base-corrections.rtcm3"
+# base_name="$(date +"%Y%m%d-%H%M%S")"
+dir="/home/pi/arch_rtk"
+suffix="-ubx-nmea-data.ubx"
 
 # Menu
 PS3='Elige tu opción: '
-options=("Enviar correcciones a receptor" "Enviar correcciones a receptor y transmitir soluciones por TCP" "Enviar correcciones a receptor y guardarlas localmente" "Enviar correcciones a receptor, guardarlas localmente y transmitir soluciones por TCP" "Salir")
+options=("corr->rece" "corr->rec & sol->TCP" "corr->rece & sol->TCP & sol->arch" "Salir")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Enviar correcciones a receptor")
+        "corr->rece")
             echo "Seleccionado: $opt"
-            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyACM0:115200:8:n:1
+            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyS0:38400:8:n:1
             break
             ;;
-        "Enviar correcciones a receptor y transmitir soluciones por TCP")
+        "corr->rec & sol->TCP")
             echo "Seleccionado: $opt"
-            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyACM0:115200:8:n:1:#3001
+            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyS0:38400:8:n:1 &\
+             /home/pi/RTKLIB/app/str2str/gcc/str2str -in serial://ttyACM0:115200:8:n:1 -out tcpsvr://localhost:3001
             break
             ;;
-        "Enviar correcciones a receptor y guardarlas localmente")
+        "corr->rece & sol->TCP & sol->arch")
             echo "Seleccionado: $opt"
-            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyACM0:115200:8:n:1 -out file:///home/pi/$corr_name
-            break
-            ;;
-        "Enviar correcciones a receptor, guardarlas localmente y transmitir soluciones por TCP")
-            echo "Seleccionado: $opt"
-            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyACM0:115200:8:n:1#3001 -out file:///home/pi/$corr_name
+            /home/pi/RTKLIB/app/str2str/gcc/str2str -in ntrip://RDSD@148.103.189.6:5020/test -px 2078678.9081 -5683737.3052 2006886.9294  -out serial://ttyS0:38400:8:n:1 &\
+             /home/pi/RTKLIB/app/str2str/gcc/str2str -in serial://ttyACM0:115200:8:n:1 -out tcpsvr://localhost:3001 -out file://$dir/"$(date +"%Y%m%d-%H%M%S")"$suffix
             break
             ;;
         "Salir")
